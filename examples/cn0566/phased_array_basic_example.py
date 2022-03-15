b1ebe91810641e673a5cf5e52407be16532edc57
Mark Thoren <mark.thoren@analog.com>
2022-03-14 17:18:16 -0700

    Add license, more comments

--- examples/cn0566/phased_array_basic_example.py
+++ examples/cn0566/phased_array_basic_example.py
@@ -1,3 +1,48 @@
+#!/usr/bin/env python3
+#  Must use Python 3
+# Copyright (C) 2022 Analog Devices, Inc.
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#     - Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     - Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#     - Neither the name of Analog Devices, Inc. nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#     - The use of this software may or may not infringe the patent rights
+#       of one or more patent holders.  This license does not release you
+#       from the requirement that you obtain separate licenses from these
+#       patent holders to use this software.
+#     - Use of the software either in source or binary form, must be run
+#       on or directly connected to an Analog Devices Inc. component.
+#
+# THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED.
+#
+# IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, INTELLECTUAL PROPERTY
+# RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+Basic utility script for working with the CN0566 "Phaser" board. Accepts the following
+command line arguments:
+
+plot - plot beam pattern, rectangular element weighting. If cal files are present,
+       they will be loaded.
+
+cal - perform both gain and phase calibration, save to files.
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -146,8 +191,10 @@ if func == "cal":
           of the aryay, then press enter...")
     print("Calibrating Gain, verbosely, then saving cal file...")
     my_cn0566.gain_calibration(verbose = True)   # Start Gain Calibration
+    my_cn0566.save_gain_cal() # Default filename
     print("Calibrating Phase, verbosely, then saving cal file...")
     my_cn0566.phase_calibration(verbose = True)  # Start Phase Calibration
+    my_cn0566.save_phase_cal() # Default filename
     print("Done calibration")
 
 if func == "plot":
