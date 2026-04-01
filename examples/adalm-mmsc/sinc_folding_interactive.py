ed4e430dcf6ac4c360d7481137c51256e1e6cb1f
Raya Mangahas <Rayanorelle.mangahas@analog.com>
2026-04-01 13:33:44 +0800

    For Checking

--- examples/adalm-mmsc/sinc_folding_interactive.py
+++ examples/adalm-mmsc/sinc_folding_interactive.py
@@ -1,7 +1,35 @@
-# Copyright (C) 2025 Analog Devices, Inc.
+# Copyright (C) 2022 Analog Devices, Inc.
 #
-# SPDX short identifier: ADIBSD
-# Author: Ioan Dragomir (ioan.dragomir@analog.com)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#     - Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     - Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#     - Neither the name of Analog Devices, Inc. nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#     - The use of this software may or may not infringe the patent rights
+#       of one or more patent holders.  This license does not release you
+#       from the requirement that you obtain separate licenses from these
+#       patent holders to use this software.
+#     - Use of the software either in source or binary form, must be run
+#       on or directly connected to an Analog Devices Inc. component.
+#
+# THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED.
+#
+# IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, INTELLECTUAL PROPERTY
+# RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import argparse
 import sys
@@ -30,7 +58,7 @@ def scan_m2k_devices():
     try:
         contexts = libm2k.getAllContexts()
         return contexts if contexts else []
-    except:
+    except Exception:
         return []
 
 
@@ -336,7 +364,7 @@ class IIOThread(Thread):
                         f"Limiting waveform to M2K ±{M2K_PEAK_V} V (applied {20*np.log10(scale):.2f} dB)."
                     )
                 self.status(
-                    f"Pushing: NSD={self.selected_nsd:.2e} V/Hz ({getattr(self, 'selected_nsd_db', 0.0):.1f} dB)  RMS={np.std(waveform):.3f} V  peak={peak_v:.3f} V  (M2K limit ±{M2K_PEAK_V} V)"
+                    f"Pushing: NSD={self.selected_nsd:.2e} V/Hz ({self.selected_nsd_db:.1f} dB)  RMS={np.std(waveform):.3f} V  peak={peak_v:.3f} V  (M2K limit ±{M2K_PEAK_V} V)"
                 )
                 aout.push([waveform, waveform * -1.0])
                 transmitting_center_frequency = self.selected_center_frequency
